21e981f4076a219d8032615dfe2237b4d1499292
Fardeen S <fardeen.shaikh@hotmail.com>
2023-10-15 23:13:57 +0530

    Added File for Reversing Linked List

---
+++ LinkedList/reversell.cpp
@@ -0,0 +1,20 @@
+// Code To reverse a Linked List in C++;
+
+#include <iostream>
+
+class node{
+public:
+  int data;
+  node* next;
+
+  node(int val){
+    data = val;
+    next = NULL;
+  }
+};
+
+
+
+int main(){
+
+}
